642434a8870b06156d70bb85e23298726269347e
VictorDev1986 <VictorDev1986@gmail.com>
2024-11-14 02:49:49 -0500

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+
+
+// Función para manejar el cambio de color del header al hacer scroll
+const header = document.getElementById('header');
+const menuToggle = document.getElementById('menu-toggle');
+const mobileMenu = document.getElementById('mobile-menu');
+const menuIconOpen = document.getElementById('menu-icon-open');
+const menuIconClose = document.getElementById('menu-icon-close');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        header.classList.add('bg-white', 'shadow-lg');
+        header.classList.remove('bg-[#0D1B2A]');
+        document.querySelectorAll('#header a').forEach(link => {
+            link.classList.remove('text-white');
+            link.classList.add('text-black');
+        });
+    } else {
+        header.classList.remove('bg-white', 'shadow-lg');
+        header.classList.add('bg-[#0D1B2A]');
+        document.querySelectorAll('#header a').forEach(link => {
+            link.classList.remove('text-black');
+            link.classList.add('text-white');
+        });
+    }
+});
+
+// Función para manejar el menú móvil
+menuToggle.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+    menuIconOpen.classList.toggle('hidden');
+    menuIconClose.classList.toggle('hidden');
+});
+
+
+
